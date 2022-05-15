3445389df325aea164723136810496dab8bdc9de
Tobias Froehlich <tobias.froehlich@rwth-aachen.de>
2022-05-15 15:55:23 +0200

    Epsilon with accent.

--- src/apis/userApi.js
+++ src/apis/userApi.js
@@ -11,7 +11,7 @@ const url = process.env.VUE_APP_BACKEND_URL
 
 const minimalNameLength = 3
 const maximalNameLength = 16
-const validCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZäöußÄÖÜẞΑΆαάΒβΓγΔδΕεΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσςΤτΥυΦφΧχΨψΩω";
+const validCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZäöußÄÖÜẞΑΆαάΒβΓγΔδΕΈεέΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσςΤτΥυΦφΧχΨψΩω";
 
 const minimalPasswordLength = 4
 const maximalPasswordLength = 32
